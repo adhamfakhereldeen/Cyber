742107ac05500ac982fd1d721e026ea4b36eec3d
adhamfakhereldeen <adhamfa321@gmail.com>
2026-01-13 22:28:53 +0200

    Delete

--- clinic_is/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .person import Person
-from .patient import Patient
-from .doctor import Doctor
-from .appointment import Appointment
-from .user import User, Admin
-from .security_mixins import SerializableMixin, AuditableMixin
